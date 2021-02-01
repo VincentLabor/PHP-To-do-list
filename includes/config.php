2c29e9ed5dba319919f290f3721cc2bdfd583863
VincentLabor <vlabor95@gmail.com>
2021-02-01 14:43:19 -0800

    Passwords are now hashed

--- includes/config.php
+++ includes/config.php
@@ -18,35 +18,3 @@ if($pdo){
     // report error message
     die("Error: Could not connect." . $e->getMessage());
    }
-
-// $con = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPass");
-
-// if (isset($_POST['submit'])) {
-//     $uname = $_POST['usersname'];
-//     $pword = $_POST['password'];
-
-//     if (empty(trim($_POST['usersname']))) {
-//         $username_err = "Please enter a username";
-//          echo '<p>Please enter a username</p>';
-
-//         // echo '<script language="javascript">';
-//         // echo 'alert("Please enter a username")';
-//         // echo '</script>';
-    
-//     } else if (empty(trim($_POST['password']))) {
-//         $password_err = "Please enter a password";
-//         echo '<p>Please enter a password</p>';
-//     } else {
-
-//         $sqlUname = "SELECT * FROM useracct WHERE user_name=$1";
-
-//         $result = pg_prepare($con, "my_query", $sqlUname);
-//         $result = pg_execute($con, "my_query", array($uname));
-        
-
-//         $query = pg_query($con, "INSERT INTO useracct(user_name, pass_word) VALUES ('$uname', '$pword');");
-//         if ($query) {
-//             echo "Record successfully added";
-//         }
-//     }
-// }
--- register.php
+++ register.php
@@ -7,15 +7,76 @@ $username = $password = $password2 = "";
 $username_err = $password_err = $password2_err = "";
 
 if (isset($_POST['submit'])) {
-    if (empty(trim($_POST["usersname"]))) {
+    //This refers to the name of an input.
+    if (empty(trim($_POST["username"]))) {
+        //This will display an error if the user fails to display a name
         $username_err = "Please enter a username";
-        // echo "Please enter a username";
     } else {
-        $sql = "SELECT * FROM useraccts WHERE user_name = :usersname";
+        //This is a database query; Will utilize a prepare and execute.
+        $sql = "SELECT * FROM useracct WHERE user_name = :username";
 
         if ($stmt = $pdo->prepare($sql)) {
-            $stmt->bindParam(":usersname", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            //The bind basically is replacing the paramater with a real value.
+            //$param_username should be coming from a form below.
             //PDO::PARAM_STR = Represents the SQL CHAR, VARCHAR, or other string data type.
+
+            $param_username = trim($_POST["username"]);
+
+            if ($stmt->execute()) {
+                //If there is a row, the username already exists in the database.
+                if ($stmt->rowCount() == 1) {
+                    $username_err = "This username is already taken";
+                } else {
+                    $username = trim($_POST["username"]);
+                }
+            }
+        } else {
+            echo "Oops! Something went wrong. Please try again later";
+        }
+
+        unset($stmt);
+    }
+
+    //Password Validation
+    //Check if there is a password entered
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password";
+        //Checks if the password is at least 6 characters
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+        $password_err = "Password must have atleast 6 characters \n";
+        //Removes the whitespace in the entered password;
+    } else {
+        $password = trim($_POST["password"]);
+    };
+
+    //Validate Password2
+    if (empty(trim($_POST["password2"]))) {
+        $password2_err = "Please enter confirm password\n";
+    } else {
+        $password2 = trim($_POST["password2"]);
+        if (empty($password_err) && ($password != $password2)) {
+            $password2_err = "Passwords did not match";
+        }
+    }
+
+    //Preparing an insert statement.
+    if (empty($username_err) && empty($password_err) && empty($password2_err)) {
+        $sql = "INSERT INTO useracct(user_name, pass_word) VALUES (:username,:password)";
+    }
+
+    if ($stmt = $pdo->prepare($sql)) {
+        $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+        $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+
+        //Set the parameters here
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+        if ($stmt->execute()) {
+            header("location: register.php");
+        } else {
+            echo "Something went wrong. Please try again later.";
         }
     }
 }
@@ -49,15 +110,20 @@ if (isset($_POST['submit'])) {
             <form action="" method="POST" class="pad2em blBorder">
                 <h2 class="centerText">Register</h2>
                 <p>Username</p>
-                <input type="text" name="usersname">
+                <input type="text" name="username" value="<?php echo $username;  ?>">
+                <?php echo $username_err; ?>
                 <p>Password</p>
                 <input type="password" name="password">
+                <?php echo $password_err; ?>
                 <p>Confirm Password</p>
                 <input type="password" name="password2">
+                <?php echo $password2_err; ?>
 
                 <input type="submit" name="submit" value="submit" class="submitBtn" />
                 <?php
-                echo $username_err;
+
+
+
                 ?>
             </form>
 
@@ -69,21 +135,3 @@ if (isset($_POST['submit'])) {
 </html>
 
 <?php
-
-// echo  phpinfo(); 
-
-// $db = pg_connect("host=localhost port=5432 dbname=test user=postgres password=FaLLaCY95!SQL");
-// if ($db == true){
-//     echo "The database is connected\n";
-// }
-
-// $query = "SELECT * FROM useracct";
-// $rs = pg_query($db, $query) or die("Cannot execute query: $query\n");
-
-// while ($row = pg_fetch_row($rs)) {
-//     echo "$row[0] $row[1] $row[2] $row[3] $row[4] <br/>";
-// }
-
-// pg_close($db);
-
-?>
