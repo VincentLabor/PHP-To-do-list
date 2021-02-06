b1cffc4c3347f344aff9ff7c9f0b0e5603c948f2
VincentLabor <vlabor95@gmail.com>
2021-02-05 21:43:44 -0800

    Can now add items to list

--- welcome.php
+++ welcome.php
@@ -1,41 +1,37 @@
 <?php
 include_once('header.php');
-require "./includes/config.php";
-
+include "./includes/config.php";
 session_start();
 
 $task = $complete = $user_associated = "";
-$task_err = $complete_err = $user_associated_err = "";
+        $task_err = $complete_err = $user_associated_err = "";
 
-echo $_SESSION["username"];
-
-if (isset($_POST['submit'])) {
-    if (empty(trim($_POST["task"]))) {
-        $task_err = "Please enter a task";
-    } else {
-        $task = trim($_POST["task"]);
-    }
-
-    if (empty($task_err)) {
-        $sql = "INSERT INTO tasklist(task) VALUES (:task)";
-
-        if ($stmt = $pdo->prepare($sql)) {
-            $stmt->bindParam(":task", $param_task, PDO::PARAM_STR);
-
-            $param_task = $task;
-
-            if ($stmt->execute()) {
-                echo "Task has been sent";
+        if (isset($_POST['submit'])) {
+            if (empty(trim($_POST["task"]))) {
+                $task_err = "Please enter a task";
             } else {
-                echo "There has been a problem. Please wait a moment and try again later.";
+                $task = trim($_POST["task"]);
             }
-            unset($stmt);
-        }
-    }
-    unset($pdo);
-}
 
-//Checkbox should only be displayed when the whole tasklist is being displayed. 
+            if (empty($task_err)) {
+                $sql = "INSERT INTO tasklist(task) VALUES (:task)";
+
+                if ($stmt = $pdo->prepare($sql)) {
+                    $stmt->bindParam(":task", $param_task, PDO::PARAM_STR);
+
+                    $param_task = $task;
+
+                    if ($stmt->execute()) {
+                        header("location: welcome.php");
+                    } else {
+                        //For some reason stmt fails but still works?
+                         echo "There has been a problem. Please wait a moment and try again later.";
+                    }
+                    unset($stmt);
+                }
+            }
+            unset($pdo);
+        }
 
 ?>
 
@@ -47,20 +43,25 @@ if (isset($_POST['submit'])) {
         <h3>To Do List</h3>
         <p>
             <?php
-
-                foreach($pdo->query('SELECT * FROM tasklist') as $row){
-                    print $row['task'] . "\t";
-                }
-
-                // $stmt = $pdo->prepare('SELECT * FROM tasklist');
-                // if($stmt->execute()){
-                //     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                //     print_r($result);
-                // }
+            include "./includes/config.php";
+            //Through each iteration, each value of the element is set to $row.
+            //Next to each row is a checkbox concatenated. 
+            foreach ($pdo->query('SELECT * FROM tasklist') as $row) {
+                print nl2br($row['task'] . '<input type="checkbox" name="" id="">' . "\n");
+            }
             ?>
         </p>
+
         <input type="text" name="task">
         <button type="submit" name="submit">Add Task</button>
+
+        <!-- PHP: Check if task has been sent properly -->
+
+        <?php
+        include "./includes/config.php";
+         ?>
+
+
     </form>
 </div>
 </main>
